2146a7ac094253886a7fa49d21362aab57ba1342
Samuel Ribas <samuelfribas@gmail.com>
2025-09-22 15:22:41 -0300

    Update ex2.asm

--- List_2/ex2.asm
+++ List_2/ex2.asm
@@ -17,4 +17,19 @@ main:
 	 li $v0, 4		# le 4 em $v0 para syscall printar string
 	 la $a0, prompt1	# carrega endereço de prompt1 em %a0 como argumento pra syscall
 	 syscall
- 
+	 
+	 li $v0, 5		# le 5 em $v0 para syscall ler um inteiro
+	 syscall		
+	 move $v0, $t0		# move o inteiro lido de $v0 para $t0
+	 
+	 
+	 li $v0, 4
+	 la $a0, prompt2
+	 syscall
+	 
+	 li $v0, 5
+	 syscall 
+	 move $v0, $t1
+	 
+funcZero
+	bne $t0, $zero, 
